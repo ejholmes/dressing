9742d10e39d2a4d84d102d664f5b326534e6e6d3
Eric J. Holmes <eric@ejholmes.net>
2013-10-18 12:46:17 -0700

    Retry methods.

--- lib/dressing/capybara/driver.rb
+++ lib/dressing/capybara/driver.rb
@@ -1,6 +1,38 @@
 module Dressing
   module Capybara
     class Driver < ::Capybara::Selenium::Driver
+      RETRY_ON = [
+        ::Selenium::WebDriver::Error::UnhandledError,
+        ::Selenium::WebDriver::Error::UnknownError
+      ]
+
+      MAX_RETRIES = 3
+
+      RETRY_METHODS = [
+        :visit, :current_url, :reset!, :within_frame, :within_window,
+        :find_window, :execute_script, :evaluate_script, :find_css, :find_xpath,
+        :html
+      ]
+
+      RETRY_METHODS.each do |name|
+        alias_method :"_base_#{name}", name
+
+        define_method name do |*args, &block|
+          retries = MAX_RETRIES
+
+          begin
+            __send__(:"_base_#{name}", *args, &block)
+          rescue *RETRY_ONE => e
+            if retries > 0
+              retries -= 1
+              puts "Received an exception (#{e}), retrying."
+              retry
+            end
+            raise
+          end
+        end
+      end
+
       def initialize(app, options = {})
         options.merge!(
           url: Dressing.configuration.remote_url,
